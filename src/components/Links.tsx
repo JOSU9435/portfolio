2aeb9f211156b8fed33807cfc7a4c0dc6ec27871
JOSU9435 <kamalnetra702@gmail.com>
2024-08-08 19:57:35 +0530

    add dynamic navbar

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,12 +1,12 @@
-const Links = ({text, scrollTo}) => {
+const Links = ({text, scrollTo, linkStyles}) => {
 
   return (  
     <div
-    className="relative flex justify-center mx-3 my-2 text-sm font-semibold text-text-200 md:my-1 md:mx-6 md:py-2 md:text-base"
+    className={`relative flex justify-center mx-3 my-2 text-sm font-semibold text-text-200 md:my-1 md:mx-6 md:py-2 md:text-base ${linkStyles}`}
     >
-      <div className="relative before:bg-accent-100 before:h-0.5 before:w-full before:absolute before:-bottom-0.5 before:origin-top-right before:transition-transform before:duration-200 before:scale-x-0 before:hover:scale-x-100 before:hover:origin-top-left">
+      <button className="relative before:bg-accent-100 before:h-0.5 before:w-full before:absolute before:-bottom-0.5 before:origin-top-right before:transition-transform before:duration-200 before:scale-x-0 before:hover:scale-x-100 before:hover:origin-top-left">
         {text}
-      </div>
+      </button>
     </div>  
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,57 @@
+import { useInView } from "react-intersection-observer";
 import Links from "./Links";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const Nav = () => {
+
+  const [scrollUp, setScrollUp] = useState(true)
+  const lastY = useRef(window.scrollY)
+  const {ref, inView} = useInView()
+
+  const [reveal, setReveal] = useState(false)
+
+  useEffect(() => {
+    setReveal((prev) => prev || inView);
+  },[inView])
+
+  const handleScroll = useCallback(() => {
+    if(lastY.current < window.scrollY && scrollUp===true) {
+      setScrollUp(false)
+    }else if(lastY.current > window.scrollY && scrollUp===false){
+      setScrollUp(true)
+    }
+    lastY.current = window.scrollY
+  },[scrollUp])
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  },[handleScroll])
+
   return (
     <div
-      className="fixed z-50 bg-bg-200 px-2 justify-evenly flex rounded-full left-1/2 -translate-x-1/2 top-4"
+      ref={ref}
+      className={`fixed z-50 bg-bg-200 px-2 justify-evenly flex rounded-full left-1/2 -translate-x-1/2 top-4 duration-200 ${scrollUp ? 'translate-y-0' : '-translate-y-[150%]'}`}
     >
-      <Links text={"About"} />
-      <Links text={"Projects"} />
-      <Links text={"Experience"} />
-      <Links text={"Skills"} />
+      <Links 
+        text={"About"} 
+        linkStyles={`duration-500 delay-0 ${reveal && scrollUp ? 'translate-x-0 opacity-100': '-translate-x-8 opacity-0'}`}
+      />
+      <Links 
+        text={"Projects"} 
+        linkStyles={`duration-500 delay-100 ${reveal && scrollUp ? 'translate-x-0 opacity-100': '-translate-x-8 opacity-0'}`}
+      />
+      <Links 
+        text={"Experience"}
+        linkStyles={`duration-500 delay-200 ${reveal && scrollUp ? 'translate-x-0 opacity-100': '-translate-x-8 opacity-0'}`}
+      />
+      <Links 
+        text={"Skills"}
+        linkStyles={`duration-500 delay-300 ${reveal && scrollUp ? 'translate-x-0 opacity-100': '-translate-x-8 opacity-0'}`}
+      />
     </div>
   );
 }
